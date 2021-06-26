f277896646bdd9ee2da47eafd6f7ad9c40f20316
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-06-26 13:35:19 +0530

    Flipkart application search page automated using POM

--- src/test/java/com/bridgelabz/pages/test/LogoutTest.java
+++ src/test/java/com/bridgelabz/pages/test/LogoutTest.java
@@ -1,22 +1,13 @@
 package com.bridgelabz.pages.test;
 
 import com.bridgelabz.base.Base;
-import com.bridgelabz.pages.Login;
 import com.bridgelabz.pages.Logout;
-import com.bridgelabz.pages.Search;
 import org.testng.annotations.Test;
 
-public class LoginTest extends Base {
-
-    @Test
-    public void loginToApplication() throws InterruptedException {
-        Login login = new Login(driver);
-        login.getUserName();
-    }
+public class LogoutTest extends Base {
     @Test
     public static void logoutTheApplication() throws InterruptedException {
         Logout logout = new Logout(driver);
         logout.logoutTheApplication();
     }
-
 }
--- src/test/java/com/bridgelabz/pages/test/SearchTest.java
+++ src/test/java/com/bridgelabz/pages/test/SearchTest.java
@@ -1,10 +1,16 @@
 package com.bridgelabz.pages.test;
 
 import com.bridgelabz.base.Base;
+import com.bridgelabz.pages.Login;
 import com.bridgelabz.pages.Search;
 import org.testng.annotations.Test;
 
 public class SearchTest extends Base {
+    @Test
+    public void loginToApplication() throws InterruptedException {
+        Login login = new Login(driver);
+        login.getUserName();
+    }
     @Test
     public void searchInApplication() throws InterruptedException {
         Search search = new Search(driver);
