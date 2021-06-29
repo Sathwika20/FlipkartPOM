57a6a2e98b0ec8af403c4d949852ab6c65c74e3f
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-06-29 12:41:34 +0530

    Mouse Hovering to my profile

--- src/main/java/com/bridgelabz/pages/HoverToMyProfile.java
+++ src/main/java/com/bridgelabz/pages/HoverToMyProfile.java
@@ -3,20 +3,42 @@ package com.bridgelabz.pages;
 import com.bridgelabz.base.Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class HoverToMyProfile extends Base {
+
+    //@FindBy is used to find the elements by using locators
+    //xpath of the element to be hovered
+
+    @FindBy(xpath = "//div[text() = 'Sathwika']")
+    public static WebElement sathwika;
     @FindBy(xpath = "//div[text() = 'My Profile']")
-    public static WebElement myProfile;
+    public static WebElement myProfilePage;
 
-    public HoverToMyProfile(WebDriver driver){
-
-        PageFactory.initElements(driver,this);
+    //Generate a constructor
+    public HoverToMyProfile(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
-    public static void setMyProfile() throws InterruptedException {
-        myProfile.click();
+    //Actions class is used to hover on to the element
+    public static void hoverToMyProfile() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(sathwika).build().perform();
         Thread.sleep(2000);
+        actions.moveToElement(myProfilePage).click().perform();
+        Thread.sleep(3000);
     }
 
 }
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/bridgelabz/pages/test/HoverToMyProfileTest.java
+++ src/test/java/com/bridgelabz/pages/test/HoverToMyProfileTest.java
@@ -3,8 +3,6 @@ package com.bridgelabz.pages.test;
 import com.bridgelabz.base.Base;
 import com.bridgelabz.pages.HoverToMyProfile;
 import com.bridgelabz.pages.Login;
-import com.bridgelabz.pages.MouseHovering;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class HoverToMyProfileTest extends Base {
@@ -15,16 +13,10 @@ public class HoverToMyProfileTest extends Base {
     }
     @Test
     public void mouseHoverToSathwika() throws InterruptedException {
-        MouseHovering mouseHovering = new MouseHovering(driver);
-        mouseHovering.mouseHover();
+        HoverToMyProfile hover = new HoverToMyProfile(driver);
+        hover.hoverToMyProfile();
     }
-    @Test
-    public static void hoverToProfileInApplication() throws InterruptedException {
-        HoverToMyProfile hoverToMyProfile = new HoverToMyProfile(driver);
-        hoverToMyProfile.setMyProfile();
 
 
 
-
-    }
 }
