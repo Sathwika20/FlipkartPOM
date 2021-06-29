31d362a91ece6af7e21604f54dffb3fe9fbc6fdf
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-06-29 12:14:29 +0530

    Automated for search button

--- src/main/java/com/bridgelabz/pages/Search.java
+++ src/main/java/com/bridgelabz/pages/Search.java
@@ -1,21 +1,40 @@
 package com.bridgelabz.pages;
 
 import com.bridgelabz.base.Base;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Search extends Base {
+    //@FindBy is used to find the elements using locators
+    //Enter the xpath of the element
     @FindBy(xpath = "//input[@class = '_3704LK']")
     public static WebElement search;
+    @FindBy(xpath = "//span[@class = 'r85cly']")
+    public static WebElement mobiles;
+    @FindBy(xpath = "//div[text() = 'realme C21 (Cross Blue, 32 GB)']")
+    public static WebElement realmeC21;
 
+    //Generate the constructor
     public Search(WebDriver driver) {
+
         PageFactory.initElements(driver,this);
     }
+    //Give the sendKeys as the name of the element which we wanted to search
     public static void search() throws InterruptedException {
         search.sendKeys("mobiles");
+        Thread.sleep(3000);
+        mobiles.click();
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
         Thread.sleep(5000);
+        Actions action = new Actions(driver);
+        action.moveToElement(realmeC21).click().perform();
+        Thread.sleep(2000);
 
     }
 }
