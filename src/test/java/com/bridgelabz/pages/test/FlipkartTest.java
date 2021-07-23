04e9116d110ae5087f7ea408e56015213fcd2c40
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-07-23 10:00:03 +0530

    Code modified

--- src/test/java/com/bridgelabz/pages/test/FlipkartTest.java
+++ src/test/java/com/bridgelabz/pages/test/FlipkartTest.java
@@ -3,20 +3,24 @@ package com.bridgelabz.pages.test;
 import com.bridgelabz.base.Base;
 import com.bridgelabz.pages.HoverToMyProfile;
 import com.bridgelabz.pages.Login;
+import com.bridgelabz.pages.Search;
 import org.testng.annotations.Test;
 
-public class HoverToMyProfileTest extends Base {
-    @Test
+public class FlipkartTest extends Base {
+    @Test(priority = 1)
     public void loginToApplication() throws InterruptedException {
         Login login = new Login(driver);
         login.getUserName();
     }
-    @Test
+    @Test(priority = 2)
+    public void searchInApplication() throws InterruptedException {
+        Search search = new Search(driver);
+        search.search();
+    }
+    @Test(priority = 3)
     public void mouseHoverToSathwika() throws InterruptedException {
         HoverToMyProfile hover = new HoverToMyProfile(driver);
         hover.hoverToMyProfile();
     }
-
-
 
 }
--- src/test/java/com/bridgelabz/pages/test/ScreenShotsTest.java
+++ src/test/java/com/bridgelabz/pages/test/ScreenShotsTest.java
@@ -2,18 +2,21 @@ package com.bridgelabz.pages.test;
 
 import com.bridgelabz.base.Base;
 import com.bridgelabz.listeners.CustomListeners;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-//@Listeners are implemented to listen all the listeners from Customlistener class
+//@Listeners are implemented to listen all the listeners from Custom listener class
 @Listeners(CustomListeners.class)
 public class ScreenShotsTest extends Base {
+    @Description("Screenshots are taken for failed test cases")
     @Test
     public void takeScreenShotFailedTest1() {
 
         Assert.assertEquals(false,true);
     }
+    @Description("Screenshots are taken for failed test cases")
     @Test
     public void takeScreenShotFailedTest2() {
 
